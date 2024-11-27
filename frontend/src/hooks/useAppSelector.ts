2cfbe72316c0870440d383927039275764415ebb
Anderson Luiz <trkz.anderson@gmail.com>
2024-11-27 03:14:44 -0300

    feat(hooks): add custom hooks

---
+++ frontend/src/hooks/useAppSelector.ts
@@ -0,0 +1,8 @@
+import type { RootState } from '@/redux/store';
+import type { TypedUseSelectorHook } from 'react-redux';
+
+import { useSelector } from 'react-redux';
+
+const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+export default useAppSelector;
