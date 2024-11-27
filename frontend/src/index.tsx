d374ad3cefa76d5983ca56c730148ae6650c43f0
Anderson Luiz <trkz.anderson@gmail.com>
2024-11-27 03:23:06 -0300

    feat(app): configure app

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+
+import theme from './styles/theme';
+
+import 'react-toastify/dist/ReactToastify.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+      <CssBaseline />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
